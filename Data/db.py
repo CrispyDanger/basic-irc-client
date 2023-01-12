fd631a4b4bc2d0d97ae50716f9ba6de84a4843f8
CrispyDanger <crispyd2405@gmail.com>
2023-01-12 23:01:06 +0500

    Added DataBase

---
+++ Data/db.py
@@ -0,0 +1,23 @@
+
+def start():
+    from db_init import Database
+    
+    db = Database().connect()
+    print("Database initialized", db)
+    db.execute("""CREATE TABLE IF NOT EXISTS users(user_id INTEGER NOT NULL UNIQUE PRIMARY KEY,
+                                                username TEXT NOT NULL, 
+                                                password TEXT NOT NULL, 
+                                                server_id INTEGER);
+                """)
+                
+    db.execute("""CREATE TABLE IF NOT EXISTS servers(server_id INTEGER NOT NULL UNIQUE PRIMARY KEY,
+                                                    server TEXT NOT NULL, 
+                                                    port INTEGER NOT NULL, 
+                                                    channel TEXT NOT NULL,
+                                                    user_id INTEGER NOT NULL,
+                                                    FOREIGN KEY(user_id) REFERENCES users(user_id)
+                                                    ON DELETE CASCADE ON UPDATE CASCADE);""")
+    return db 
+
+if __name__ == "__main__":
+    start()
--- main.py
+++ main.py
@@ -1,73 +1,34 @@
-import sys
-import socket
-import time
-from threading import Thread
-from threading import Event
-import re
-import os
-from datetime import datetime
-import ssl
-import json
+import Data.db as database
+from irc import IRC_Client
+from uuid import uuid4
 
-data = json.load(open("config.json"))
-
-server = data["server"]
-port = data["port"]
-channel = data["channel"]
-botnick = data["botnick"]
-botpassw = data['botpassw']
-log_to_file = data["log_to_file"]
-
-def send_messages():
-    while True:
-        take = input("~: ")
-        irc.send(f"PRIVMSG {channel} :{take}\r\n".encode())
-
-def join():
-    print(f"[/] Connecting to {server}\n")
-    irc.connect((server, port))
-    irc.send(f"USER {botnick} {botnick} {botnick} :This is a fun bot!\r\n".encode())
-    irc.send(f"NICK {botnick}\r\n".encode())
-    irc.send(f"NICKSERV IDENTIFY {botpassw}\r\n".encode())
-    time.sleep(2)
-
-    irc.send(f"JOIN {channel}\r\n".encode())
-    irc.send(f"PRIVMSG {channel} :`[+] Joined.`\r\n".encode())
+db = database.start()
 
 
-def read_messages():
-    while True:
-        text = irc.recv(2040)
-        formatted = text.decode()
-            
-        if "PRIVMSG" in formatted:
-            arr = re.split("!~.*\s+PRIVMSG\s+|\s+:", formatted[1:], maxsplit=2)
-            user_messages = f"<{arr[0]}> {arr[-1]}"
-            now = datetime.now()
-            get_current_time = now.strftime("%H:%M:%S")
-            print("\n" + get_current_time + " | " + user_messages, end = "")
-            if log_to_file == "true":
-                f = open("logs.txt", "a", encoding="utf-8")
-                f.write("\n" + get_current_time + " | " + user_messages)
-                f.close()
+def get_uuid():
+    return uuid4().hex
 
-        if "PING" in formatted:
-            encoded = ('PONG ' + formatted.split()[1]).encode()
-            print(encoded)
-            irc.send(encoded)
 
-try:
-    # ctx = ssl.create_default_context()
-    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # irc = ctx.wrap_socket(sock, server_hostname=server)
+def main():
+    print("Would you like to login? [Y/N] \n")
+    inp = input()
+    if inp == "Y":
+        print("Please enter your login and password in this format <username:password>: \n")
+        login = input().split(":")
+        username = login[0]
+        password = login[1]
+        res = db.execute(f"""SELECT username FROM users WHERE username == {username} AND password == {password} """)
+        if res:
+            print("User Found!")
+        else:
+            print("User not Found!")
 
-    event = Event()
-    event.set()
-    join()
 
-    Thread(target = read_messages).start()
-    Thread(target = send_messages).start()
+if __name__ == "__main__":
+    main()
 
-except KeyboardInterrupt:
-    print("[+] Exiting...")
-    irc.send(f"PRIVMSG {channel} :`[+] Exiting...`\r\n".encode())
+
+
+
+
+
